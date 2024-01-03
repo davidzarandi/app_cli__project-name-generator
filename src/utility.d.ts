537e48ca08e3a8e553e2199f824aef36a20d97ac
Dávid J. Zarándi <noreply@zdaav.eu>
2024-01-03 17:25:26 +0100

    fix: utility.d.ts file

--- src/utility.d.ts
+++ src/utility.d.ts
@@ -1,8 +1,8 @@
-export type ValueOf<T> = T[keyof T];
+declare type ValueOf<T> = T[keyof T];
 
-export type Expect<T extends true> = T;
+declare type Expect<T extends true> = T;
 
-export type Equal<X, Y> = (<T>() => T extends X ? 1 : 2) extends <
+declare type Equal<X, Y> = (<T>() => T extends X ? 1 : 2) extends <
   T,
 >() => T extends Y ? 1 : 2
   ? true
